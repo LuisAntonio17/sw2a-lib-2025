580863ef7920ea1e0b81c7422d48a605a5fb45d8
ETEC <164@etec.sp.gov.br>
2025-05-23 08:25:22 -0300

    aula 23/05/2025

--- bibsw2/src/app/api/v1/generos/route.ts
+++ bibsw2/src/app/api/v1/generos/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
+import dados from "@/db.json";
+
 export async function GET() {
-    return NextResponse.json({"mensagem" : "Gêneros"});
+   // return NextResponse.json({"mensagem" : "Gêneros"});
+    return NextResponse.json(dados);
 }
